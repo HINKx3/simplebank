e9ddb0d1b2c8152f9c8b1d6c8ccf00437c954fef
Yuanxin Tuo <ytuo@Yuanxins-MacBook-Air.local>
2024-01-10 21:49:23 -0500

    RESTful APIs

--- api/account_test.go
+++ api/account_test.go
@@ -11,13 +11,13 @@ import (
 	"testing"
 	"time"
 
+	mockdb "github.com/HINKx3/simplebank/db/mock"
+	db "github.com/HINKx3/simplebank/db/sqlc"
+	"github.com/HINKx3/simplebank/token"
+	"github.com/HINKx3/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
-	mockdb "github.com/techschool/simplebank/db/mock"
-	db "github.com/techschool/simplebank/db/sqlc"
-	"github.com/techschool/simplebank/token"
-	"github.com/techschool/simplebank/util"
 )
 
 func TestGetAccountAPI(t *testing.T) {
