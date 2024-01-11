09481fcb83c3cf5a62c636fa71c40294e39e960d
Yuanxin Tuo <ytuo@Yuanxins-MacBook-Air.local>
2024-01-10 21:47:30 -0500

    main_test

--- api/main_test.go
+++ api/main_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 	"time"
 
+	db "github.com/HINKx3/simplebank/db/sqlc"
+	"github.com/HINKx3/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
-	db "github.com/techschool/simplebank/db/sqlc"
-	"github.com/techschool/simplebank/util"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
